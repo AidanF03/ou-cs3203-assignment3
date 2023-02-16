837c3b66d0481e0c7048ef6d578a0009463c3a9b
Aidan Foreman <Aidan.A.Foreman-1@ou.edu>
2023-02-16 13:27:10 -0600

    Added main function

--- ListSum.py
+++ ListSum.py
@@ -10,3 +10,19 @@ def arrMult(array):
     for i in array:
         totalMult *= i
     return totalMult
+
+def main():
+
+    print("Enter each number of your array(enter END to stop entering numbers)")
+    arr = []
+    while(True):
+        x = input()
+        if(x == "END"):
+            break
+        arr.append(int(x))
+    
+    print("Total of array: " + str(arrSum(arr)))
+    print("Total of array elements multiplied: " + str(arrMult(arr)))
+
+if __name__ == "__main__":
+    main()
